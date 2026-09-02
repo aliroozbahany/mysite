f260f5bcab5fee525231c7ecc894a4398afb9242
aliroozbahany <aliroozbahany@gmail.com>
2026-09-02 17:16:42 +0330

    add templates
    add templates

---
+++ website/urls.py
@@ -0,0 +1,11 @@
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    
+    path('home',views.home ),
+    path('about',views.about_view ),
+    path('contact',views.contact_view ),
+    
+]
